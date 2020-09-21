88535f950df1ea0efd6e1b04b857e5b1e3b5f8d8
KhueNgocDang <khue.dm1910442@sis.hust.edu.vn>
2020-09-21 18:35:44 +0700

    Location Tracking finished

---
+++ app/src/main/java/com/example/feander/MapsActivity.java
@@ -0,0 +1,96 @@
+package com.example.feander;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Marker marker;
+    //Use location manager to manage location
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    //double latitude, longtitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION_PERMISSION);
+        }
+        else
+            {
+                locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(@NonNull Location location) {
+                        Geocoder geocoder = new Geocoder(getApplicationContext());
+                        try {
+                            List<Address> addresses =
+                                    geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                            String loc = addresses.get(0).getLocality()+":"+addresses.get(0).getCountryName();
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            if(marker!=null)
+                            {
+                                marker.remove();
+                                marker = mMap.addMarker(new MarkerOptions().position(latLng).title(loc));
+                                mMap.setMaxZoomPreference(20);
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12.0f));
+                            }
+                            else{
+                                marker = mMap.addMarker(new MarkerOptions().position(latLng).title(loc));
+                                mMap.setMaxZoomPreference(20);
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,12.0f));
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+}
